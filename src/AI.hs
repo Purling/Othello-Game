17bc06d277e27ab3fc284019f6b58cac61ad070b
Zling Ouyang <u7312578@anu.edu.au>
2021-05-24 02:11:48 +1000

    Completed greedy AI

--- src/AI.hs
+++ src/AI.hs
@@ -21,7 +21,8 @@ data AIFunc
 -- "default" in this table is the one your tutor will dedicate most of
 -- his or her attention to marking.
 ais :: [(String, AIFunc)]
-ais = [ ("firstLegalMove", NoLookahead firstLegalMove)
+ais = [ ("firstLegalMove", NoLookahead firstLegalMove),
+        ("greedyStrategy", NoLookahead greedyAI)
       ]
 
 -- | A very simple AI, which picks the first move returned by the
@@ -30,3 +31,74 @@ ais = [ ("firstLegalMove", NoLookahead firstLegalMove)
 -- ended the game.
 firstLegalMove :: GameState -> Move
 firstLegalMove st = head (legalMoves st)
+
+-- | A greedy AI with greediness based completely on the most pieces that can
+-- be captured on the next available move.
+greedyAI :: GameState -> Move
+greedyAI (GameState bounds turn board) = case turn of
+  GameOver _ -> error "greedyAI: Invalid Input (Gameover is invalid)"
+  _          -> greedyHeuristic (legalMoves (GameState bounds turn board))
+                 (GameState bounds turn board)
+
+
+-- | A function which uses a heuristic to determine the next best move. The
+-- next best move is decided entirely on immediate score improvement.
+greedyHeuristic :: [Move] -> GameState -> Move
+greedyHeuristic [] _ = Move (9,9)
+greedyHeuristic (x:xs) (GameState bounds turn board) = case x:xs of
+  [] -> x
+  _  -> case turn of
+    GameOver _  -> error "greedyAI: Invalid Input (Gameover is invalid)"
+    Turn _ -> snd (foldr1 maxTuple (map score tupleList))
+  where
+    gameStateList :: [Maybe GameState]
+    gameStateList = map (applyMove (GameState bounds turn board)) (x:xs)
+    tupleList :: [(Maybe GameState, Move)]
+    tupleList = zip gameStateList (x:xs)
+
+
+compare :: Board -> Player -> Int
+compare board player = max (currentScore board player) 0
+
+-- (map ((applyMove (GameState bounds turn board)) (x:xs))) 
+-- Gives out a list of Maybe GameState
+
+-- map score (map ((applyMove (GameState bounds turn board)) (x:xs)))
+-- Returns a list of scores
+
+-- Gives out a tuple of lists given a list and another list
+-- zip (map (applyMove (initialState (8,8))) li) li
+
+-- Turns a Maybe GameState list into a list of ints
+-- score :: Maybe GameState -> Int
+-- score gameState = case gameState of
+--   (Just (GameState _ turn board)) -> 
+--     case turn of
+--       GameOver _  -> error "greedyAI: Invalid Input (Gameover is invalid)"
+--       Turn player -> currentScore board player
+--   _ -> -1
+
+-- Turns a tuple Maybe GameState list into a list of ints and moves
+score :: (Maybe GameState, Move) -> (Int, Move)
+score (gameState,move) = case gameState of
+  (Just (GameState _ turn board)) ->
+    case turn of
+      GameOver _  -> (0,move)
+      Turn player -> (currentScore board player, move)
+  _ -> (-1,move)
+
+-- A max function for tuples. Can be polymorphic
+maxTuple :: (Int, Move) -> (Int, Move) -> (Int, Move)
+maxTuple (a,b) (c,d)
+  | a > c = (a,b)
+  | otherwise = (c,d)
+
+li :: [Move]
+li = [Move (3,2), Move (2,3), Move (3,3)]
+
+-- Testing function: foldr max (0) (map score (map (applyMove (initialState (8,8))) li))
+-- Gives out the max int from a list of ints
+-- (map score' zip (map (applyMove (initialState (8,8))) li) li))
+-- Gives out a list of (int, move)
+-- foldr max' (0,Move (1,1)) (map score' zip (map (applyMove (initialState (8,8))) li) li))
+-- Gives out the tuple which has the most score advantage (Score, Move)
