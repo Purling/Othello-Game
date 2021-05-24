925e159f1cce57b14d33d450c38061a9484e76f6
Zling Ouyang <u7312578@anu.edu.au>
2021-05-25 02:44:23 +1000

    Created basis of MM AI

--- src/AI.hs
+++ src/AI.hs
@@ -1,7 +1,7 @@
 {-|
 Module      : AI
 Description : AIs for Othello
-Copyright   : (c) 2020 Your Name Here
+Copyright   : (c) 2020 Ziling Ouyang
 License     : AllRightsReserved
 -}
 module AI where
@@ -23,6 +23,7 @@ data AIFunc
 ais :: [(String, AIFunc)]
 ais = [ ("firstLegalMove", NoLookahead firstLegalMove),
         ("greedyStrategy", NoLookahead greedyAI)
+        -- ("minimaxStrategy", WithLookahead minMaxAI)
       ]
 
 -- | A very simple AI, which picks the first move returned by the
@@ -45,15 +46,13 @@ greedyAI (GameState bounds turn board) = case turn of
 -- next best move is decided entirely on immediate score improvement.
 greedyHeuristic :: [Move] -> GameState -> Move
 greedyHeuristic [] _ = Move (9,9) -- Techincally this is cased below
-greedyHeuristic (x:xs) (GameState bounds turn board) = case x:xs of
-  [] -> x
-  _  -> case turn of
+greedyHeuristic (x:xs) gameState@(GameState _ turn _) = case turn of
     GameOver _  -> error "greedyAI: Invalid Input (Gameover is invalid)"
-    Turn _ -> snd (foldr1 maxTuple (map score tupleList))
+    Turn _      -> snd (foldr1 maxTuple (map score tupleList))
   where
     -- | Returns a list of Maybe GameState given a egal list of moves
     gameStateList :: [Maybe GameState]
-    gameStateList = map (applyMove (GameState bounds turn board)) (x:xs)
+    gameStateList = map (applyMove gameState) (x:xs)
     -- | Returns a list of (Maybe GameState, Move) given [Maybe GameState]
     -- and [Move]
     tupleList :: [(Maybe GameState, Move)]
@@ -75,6 +74,38 @@ maxTuple (a,b) (c,d)
   | a > c = (a,b)
   | otherwise = (c,d)
 
+-- | Definition of a rose tree
+data Rose a = Rose a [Rose a]
+
+-- | A game tree for othello
+othelloTree :: (Int,Int) -> GameState -> Rose GameState
+othelloTree (acc,depth) gameState
+--Rose gameState (map (othelloTree (acc+1,depth)) (map (\(Just x) -> x) (map (applyMove gameState) (legalMoves gameState))))
+  | acc < depth = Rose gameState (map ((othelloTree (acc+1,depth) . (\(Just x) -> x)) . applyMove gameState) (legalMoves gameState))
+  | otherwise = Rose gameState []
+
+-- | Returns a score which rates each board from a given player's perspective
+returnScore :: Player -> Board -> Int
+returnScore player board
+  | player == Player1 = playerScore
+  | otherwise = -playerScore
+   where
+    playerScore = currentScore board Player1 - currentScore board Player2
+
+-- | A function which determines the most optimal move using minmax techniques
+-- given GameState and how many layers to check
+-- value :: GameState
+
+-- | Code to show RoseTrees in a nicer manner
+instance Show a => Show (Rose a) where
+  show = unlines . layout
+    where
+      layout :: Show a => Rose a -> [String]
+      layout (Rose v []) = [show v]
+      layout (Rose v children) = show v : concatMap (indent . layout) children
+      indent :: [String] -> [String]
+      indent = map ("  "++)
+
 -- | An example of a list of moves for testing
 li :: [Move]
 li = [Move (3,2), Move (2,3), Move (3,3)]
@@ -85,6 +116,7 @@ li = [Move (3,2), Move (2,3), Move (3,3)]
 -- Gives out a list of (int, move)
 -- foldr max' (0,Move (1,1)) (map score' zip (map (applyMove (initialState (8,8))) li) li))
 -- Gives out the tuple which has the most score advantage (Score, Move)
+-- map (applyMove gameState) legalMove gameState
 
 -- (map ((applyMove (GameState bounds turn board)) (x:xs))) 
 -- Gives out a list of Maybe GameState
